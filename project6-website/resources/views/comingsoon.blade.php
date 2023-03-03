b41547003a8eeb52aef08d1d14963997738c129e
ps224327 <ps224327@edu.summacollege.nl>
2023-03-03 13:23:56 +0100

    website beter

--- project6-website/resources/views/comingsoon.blade.php
+++ project6-website/resources/views/comingsoon.blade.php
@@ -18,7 +18,8 @@
                 <p class="pr-4">Zwanenburg <br> 1161 AM <br> Kruiswaal 16</p>
                 <p>Soesterberg <br> 3769 DH <br> Kampweg 47</p>
             </div>
-            <a href="{{url(' /contact)}}" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r-lg border-t border-b border-r border-blue-500 border-gray-200">Contact</a>
+        </div class="flex items-center py-2">
+            <a href="{{url('/contact')}}" class="bg-green-700 w-3 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-r-lg border-t border-b border-r border-blue-500 border-gray-200">Contact</a>
         </div>
     </div>
     <div class="absolute top-0 left-0 flex flex-col justify-center h-full w-full px-8 text-center">
@@ -27,7 +28,7 @@
         <form action="#" method="POST" class="mx-auto">
             <div class="flex items-center py-2">
                 <input type="email" name="email" placeholder="Email" class="px-4 py-2 rounded-l-lg border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white">
-                <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r-lg border-t border-b border-r border-blue-500 border-gray-200">Houd mij op de hoogte!</button>
+                <button type="submit" class="bg-green-700 hover:bg-green-600 text-white font-bold py-2 px-4 rounded-r-lg border-t border-b border-r border-blue-500 border-gray-200">Houd mij op de hoogte!</button>
             </div>
         </form>
     </div>
