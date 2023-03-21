12fc6e235075b4b96b1b96dc64f90dd267d7406a
Delano van Londen <ps221494@summacollege.nl>
2023-03-21 11:48:15 +0100

    barcode generator

--- barcode-generator/database/migrations/2023_03_17_100929_add_barcode_to_product.php
+++ barcode-generator/database/migrations/2023_03_17_100929_add_barcode_to_product.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('product', function (Blueprint $table) {
+        Schema::create('product', function (Blueprint $table) {
             $table->string('barcodes')->nullable();
         });
         
